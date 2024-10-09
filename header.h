c4fbc8a25f06d70cd39c76fe33c67e52c4a2389c
Matthew Wehrmeyer <mwehrmeyer@odin.cs.csubak.edu>
2024-10-08 18:44:06 -0700

    moved checkwalls from header to personal file, started Door class

--- header.h
+++ header.h
@@ -43,62 +43,64 @@ const Flt MINIMUM_ASTEROID_SIZE = 60.0;
 const float gameSpeed = 2.0f;
 
 class Wall {
-    public:
-        float xPos;
-        float yPos;
-        float xLen;
-        float yLen;
-		float color[3];
+public:
+    float xPos;
+    float yPos;
+    float xLen;
+    float yLen;
+	float color[3];
     
-    public:
-        Wall (float x, float y, float x_l, float y_l) {
-            xPos = x;
-            yPos = y;
-            xLen = x_l;
-            yLen = y_l;
-		    color[0] = 0.0;
-            color[1] = 1.0;
-            color[2] = 1.0;
-        }
+public:
+    Wall (float x, float y, float x_l, float y_l) {
+        xPos = x;
+        yPos = y;
+        xLen = x_l;
+        yLen = y_l;
+	    color[0] = 0.0;
+        color[1] = 1.0;
+        color[2] = 1.0;
+    }
+};
+
+class Door {
+public:
+    //int id;
+    float doorPos[2];
+    float size[2];
+    int toRoom;
+    float color[3];
+public:
+    Door (float x, float y, float x_l, float y_l, int rID) {
+        doorPos[0] = x;
+        doorPos[1] = y;
+        size[0] = x_l;
+        size[1] = y_l;
+        toRoom = rID;
+        color[0] = 0.1f;
+        color[1] = color[2] = 0.0f;
+    }
 
 };
 
 class Room {
-    public:
-		int id;
-        std::vector<Wall> walls;
+public:
+	int id;
+    std::vector<Wall> walls;
+    std::vector<Door> doors;
 
-    public:
-        Room() {
-            //id = 0;          
-            //walls.push_back( Wall(50.0f, 100.0f, 50.0f, 50.0f));
-            //walls.push_back( Wall(75.0f, 200.0f, 100.0f, 50.0f));
-            //walls.push_back( Wall(200.0f, 200.0f, 30.0f, 50.0f));
-            //walls.push_back( Wall(400.0f, 50.0f, 200.0f, 100.0f));
-            
-        }
+public:
+    Room() {
+        //id = 0;          
+        //walls.push_back( Wall(50.0f, 100.0f, 50.0f, 50.0f));
+        //walls.push_back( Wall(75.0f, 200.0f, 100.0f, 50.0f));
+        //walls.push_back( Wall(200.0f, 200.0f, 30.0f, 50.0f));
+        //walls.push_back( Wall(400.0f, 50.0f, 200.0f, 100.0f));            
+    }
 
-		Room(int i, std::vector<Wall> w) {
-			id = i;
-			//while (i < size)
-			//{
-			walls = w;
-			//}
+	Room(int i, std::vector<Wall> w) {
+		id = i;
+		walls = w;
+    }
 
-		}
-
-        int checkWall(float newPos[2]) {
-            int blocked = 0;
-            int i = 0;
-            while (!blocked && i < (int)walls.size()) {
-                if (newPos[0] > walls[i].xPos && newPos[0] < walls[i].xPos + walls[i].xLen && newPos[1] > walls[i].yPos && newPos[1] < walls[i].yPos + walls[i].yLen) {
-                    blocked = 1;
-                }
-                i++;
-            }
-
-            return blocked;
-        }
-
-        ~Room() {}
+    ~Room() {}
 };
---
+++ mwehrmeyer.cpp
@@ -0,0 +1,54 @@
+#include "header.h"
+
+// Builds the room to be drawn for any objects in the room
+// This determines the rooms walls.
+
+// World mapping
+// Includes walls and other objects
+Room rooms[] = { 
+    Room(0, {Wall(50.0f, 100.0f, 50.0f, 50.0f),
+             Wall(75.0f, 200.0f, 100.0f, 50.0f),
+             Wall(200.0f, 200.0f, 30.0f, 50.0f),
+             Wall(400.0f, 50.0f, 200.0f, 100.0f)},
+             {Door(50.0f, 150.0f, 50.0f, 50.0f, 1)}), 
+    Room(1, {Wall(100.0f, 100.0f, 50.0f, 50.0f),
+             Wall(175.0f, 200.0f, 100.0f, 50.0f),
+             Wall(300.0f, 100.0f, 30.0f, 50.0f),
+             Wall(400.0f, 450.0f, 75.0f, 100.0f),
+             Wall(400.0f, 150.0f, 75.0f, 100.0f)})
+};
+
+// Initializes first room to make
+Room startRooms(int roomID) {
+    Room r = rooms[roomID];
+    return r;
+}
+
+// Saves data of current room back into Room array
+void roomSave(Room cur)
+{
+    rooms[cur.id] = cur;
+}
+
+// Will from one room to the next based on interacting with a door
+Room swapRoom(int roomID, Room current) {
+    Room next;
+    roomSave(current);
+    next = rooms[roomID];
+    return next;
+}
+
+// Will check if the player is colliding with a wall to block any movement
+int checkWall(float newPos[2], Room room) {
+    std::vector<Wall> w = room.walls;
+    int blocked = 0;
+    int i = 0;
+    while (!blocked && i < (int)w.size()) {
+        if (newPos[0] > w[i].xPos && newPos[0] < w[i].xPos + w[i].xLen && 
+            newPos[1] > w[i].yPos && newPos[1] < w[i].yPos + w[i].yLen) {
+            blocked = 1;
+        }
+        i++;
+    }
+    return blocked;
+}
