2da66498057531a77cbad923443edcfb597dbf06
Adrian R. <148797592+Loon01@users.noreply.github.com>
2024-10-24 16:24:51 -0700

    Changed Zombie array to vector

--- arodriguez3.cpp
+++ arodriguez3.cpp
@@ -13,31 +13,22 @@
 #include "header.h"
 
 //Vector for Zombies 
-/*
 std::vector<Zombie> zombies = {
 	Zombie(0, 575.0f, 400.0f, 0),
 	Zombie(1, 75.0f, 100.0f, 0),
 	Zombie(0, 575.0f, 400.0f, 1),
 	Zombie(1, 375.0f, 200.0f, 1)
 };
-*/
 
-//Array for Zombies
-Zombie zombies[] = {
-	Zombie(0, 575.0f, 400.0f, 0),
-	Zombie(1, 75.0f, 100.0f, 0),
-	Zombie(0, 575.0f, 400.0f, 1),
-	Zombie(1, 375.0f, 200.0f, 1)
-};
 
 //Drawing/Rendering Zombies
-void renderZombie(int numZombies, Room current) 
+void renderZombie(Room current) 
 {
-	for (int i = 0; i < numZombies; i++) {
+	for (size_t i = 0; i < zombies.size(); i++) {
 		if (zombies[i].room == current.id) {
 			glPushMatrix();
-			glTranslatef(zombies[i].pos[0], zombies[i].pos[1], zombies[i].pos[2]);
-			glRotatef(zombies[i].angle, 0.0f, 0.0f, 1.0f);
+			glTranslatef(zombies[i].pos[0], zombies[i].pos[1], 0.0f);
+			glRotatef(0.0f, 0.0f, 0.0f, 1.0f);
 			float size = 9.0f;
 			glBegin(GL_QUADS);
 				glColor3f(0.0f, 1.0f, 0.0f);
