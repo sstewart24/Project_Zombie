1de7da03ab293e225b2f0dc63c4df1d094f937a5
Adrian R. <148797592+Loon01@users.noreply.github.com>
2024-10-24 15:54:21 -0700

    Created Array of Zombies to have different positions in each room

--- arodriguez3.cpp
+++ arodriguez3.cpp
@@ -1,46 +1,58 @@
-#include "header.h"
-
 /*
-Make zombies that roam around the room in a random way, 
-look at asteroids and how it randomly moves around the room
+* Adrian's source file
+*
+* Currently:
+*	render Zombies into rooms and have them in different positions
+*	depending on what room they're in.
+*
+*
+*
+*
 */
 
-extern Zombie zombie;
+#include "header.h"
 
-void renderZombie(Zombie zombie) //Drawing/Rendering Zombies
+//Vector for Zombies 
+/*
+std::vector<Zombie> zombies = {
+	Zombie(0, 575.0f, 400.0f, 0),
+	Zombie(1, 75.0f, 100.0f, 0),
+	Zombie(0, 575.0f, 400.0f, 1),
+	Zombie(1, 375.0f, 200.0f, 1)
+};
+*/
+
+//Array for Zombies
+Zombie zombies[] = {
+	Zombie(0, 575.0f, 400.0f, 0),
+	Zombie(1, 75.0f, 100.0f, 0),
+	Zombie(0, 575.0f, 400.0f, 1),
+	Zombie(1, 375.0f, 200.0f, 1)
+};
+
+//Drawing/Rendering Zombies
+void renderZombie(int numZombies, Room current) 
 {
-	glPushMatrix();
-	glTranslatef(zombie.pos[0], zombie.pos[1], zombie.pos[2]);
-	glRotatef(zombie.angle, 0.0f, 0.0f, 1.0f);
-	float size = 9.0f;
-	glBegin(GL_QUADS);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(size, size, 0.0f); //top right
-		glVertex3f(size, -size, 0.0f); //bottom right
-		glVertex3f(-size, -size, 0.0f); //bottom left
-		glVertex3f(-size, size, 0.0f); //top left
-	glEnd();
+	for (int i = 0; i < numZombies; i++) {
+		if (zombies[i].room == current.id) {
+			glPushMatrix();
+			glTranslatef(zombies[i].pos[0], zombies[i].pos[1], zombies[i].pos[2]);
+			glRotatef(zombies[i].angle, 0.0f, 0.0f, 1.0f);
+			float size = 9.0f;
+			glBegin(GL_QUADS);
+				glColor3f(0.0f, 1.0f, 0.0f);
+				glVertex3f(size, size, 0.0f); //top right
+				glVertex3f(size, -size, 0.0f); //bottom right
+				glVertex3f(-size, -size, 0.0f); //bottom left
+				glVertex3f(-size, size, 0.0f); //top left
+			glEnd();
 
-	glBegin(GL_POINTS);
-	glVertex2f(0.0f, 0.0f);
-	glEnd();
-	glPopMatrix();
-}
-
-//const int MAX_ZOMBIES = 2; //number of zombies
-Zombie zombies[MAX_ZOMBIES];
-
-void init_zombies(Zombie zombies[], int numZombies) { //function for spawning zombies
-	for (int i = 0; i < numZombies; ++i) {
-		zombies[i].pos[0] = 100.0f + (i * 50.0f);
-		zombies[i].pos[1] = 200.0f;
-		//zombies[i].angle = 10.0f;
-		zombies[i].id = i;
+			glBegin(GL_POINTS);
+			glVertex2f(0.0f, 0.0f);
+			glEnd();
+			glPopMatrix();
+		}
 	}
-
 }
 
-//void roam_zombies() {}
-
-//extern Zombie zombie;
-
+//int Zcollision() {}
