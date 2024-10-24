e5c89d4c2b6f633e47c6b98accce769892e40548
Adrian R. <148797592+Loon01@users.noreply.github.com>
2024-10-24 15:57:39 -0700

    Made changes in Zombie class

--- header.h
+++ header.h
@@ -22,8 +22,8 @@
 #define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2]
 #define VecDot(a,b)	((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
 #define VecSub(a,b,c) (c)[0]=(a)[0]-(b)[0]; \
-                             (c)[1]=(a)[1]-(b)[1]; \
-                             (c)[2]=(a)[2]-(b)[2];
+						(c)[1]=(a)[1]-(b)[1]; \
+						(c)[2]=(a)[2]-(b)[2];
 
 //-----------------------------------------------------------------------------
 //Setup timers
@@ -59,15 +59,15 @@ class Wall {
         float yPos;
         float xLen;
         float yLen;
-        float color[3];
-
+		float color[3];
+    
     public:
         Wall (float x, float y, float x_l, float y_l) {
             xPos = x;
             yPos = y;
             xLen = x_l;
             yLen = y_l;
-            color[0] = 0.0;
+		    color[0] = 0.0;
             color[1] = 1.0;
             color[2] = 1.0;
         }
@@ -75,77 +75,84 @@ class Wall {
 };
 
 class Door {
-    public:
-        int id;
-        float xPos;
-        float yPos;
-
-        // Define which roomID that the door will go to
-        int toRoom;
-        // If there are multiple doors in that room, 
-        // it will put the player next to the right door it went through
-        int toDoor;
-        int facing;
-        float color[3];
-    public:
-        Door (int idD, float x, float y, int dID, int rID, int direction) {
-            id = idD;
-            xPos = x;
-            yPos = y;
-            toDoor = dID;
-            toRoom = rID;
-            facing = direction;
-            color[0] = 0.647059f;
-            color[1] = color[2] = 0.164706f;
-        }
+public:
+    int id;
+    float xPos;
+    float yPos;
+    // Define which roomID that the door will go to
+    int toRoom;
+    // If there are multiple doors in that room, 
+    // it will put the player next to the right door it went through
+    int toDoor;
+    int facing;
+    float color[3];
+public:
+    Door (int idD, float x, float y, int dID, int rID, int direction) {
+        id = idD;
+        xPos = x;
+        yPos = y;
+        toDoor = dID;
+        toRoom = rID;
+        facing = direction;
+        color[0] = 0.647059f;
+        color[1] = color[2] = 0.164706f;
+    }
 
 };
 
-    //number of max of Zombies
-    #define MAX_ZOMBIES 2
+//number of max Zombies
+#define MAX_ZOMBIES 10
 
-
-class Zombie { //Zombie Class
+//Zombie Class
+class Zombie { 
     public: //Access specifier
-        int id; // Identification of the zombie
+		int id; // Identification of the zombie
         int room; // Which room it is in
         int xPos, yPos;
-        Vec pos;
-        //Vec dir;
-        float angle;
-        float color[3];
-        //static int numZombies; //num of Zombies
+		Vec pos;
+	    //Vec dir;
+	    float angle;
+	    float color[3];
+        
         //std::vector<Zombie> zombies;
 
 
     public:
-        Zombie() { //Zombie Constructor 
-            xPos = 640;
-            yPos = 480;
-            pos[0] = (Flt)(xPos/2);
-            pos[1] = (Flt)(yPos/2);
-            pos[2] = 0.0f;
-            //VecZero(dir);
-            //angle = 0.0;
+        //Zombie Constructor 
+        
+        Zombie() { 
+        
+			//xPos = 640;
+			//yPos = 480;
+			//pos[0] = (Flt)(xPos/2);
+			//pos[1] = (Flt)(yPos/2);
+			//pos[2] = 0.0f;
 
-
-
-            //xPos = x;
-            //yPos = y;
-            //dir = d;
+		    //color[0] = 0.0f;
+			//color[1] = 1.0f;
+			//color[2] = 0.0f;
+         
+        }
+        
+        //Zombie Constructor
+        Zombie(int idZ, float x, float y, int r) {
+            id = idZ;
+			pos[0] = x;
+			pos[1] = y;
+            //pos[2] = 0.0f;
             //angle = a;
-
-            color[0] = 0.0f;
-            color[1] = 1.0f;
-            color[2] = 0.0f;
+            room = r;
+		    color[0] = 0.0f;
+			color[1] = 1.0f;
+			color[2] = 0.0f;
         }
 
-        ~Zombie() {}
+		~Zombie() {}
 };
 
 class Room {
     public:
-        int id;
+		int id;
         std::vector<Wall> walls;
         std::vector<Door> doors;
         //std::vector<Zombie> zombies;
@@ -157,18 +164,19 @@ class Room {
             //walls.push_back( Wall(75.0f, 200.0f, 100.0f, 50.0f));
             //walls.push_back( Wall(200.0f, 200.0f, 30.0f, 50.0f));
             //walls.push_back( Wall(400.0f, 50.0f, 200.0f, 100.0f));
-
+            
         }
 
-        Room(int i, std::vector<Wall> w, std::vector<Door> d) {
-            id = i;
-            //while (i < size)
-            //{
-            walls = w;
+		Room(int i, std::vector<Wall> w, std::vector<Door> d) {
+			id = i;
+			//while (i < size)
+			//{
+			walls = w;
             doors = d;
-            //}
+            //zombies = z;
+			//}
 
-        }
+		}
 
         ~Room() {}
 };
