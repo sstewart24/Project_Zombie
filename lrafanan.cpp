58f2d02825aba6490f11b294a80e6af188bec77b
LyanneMarie Rafanan <lrafanan@odin.cs.csubak.edu>
2024-11-12 17:53:20 -0800

    added stuff to render inventory box prettier

--- lrafanan.cpp
+++ lrafanan.cpp
@@ -3,22 +3,15 @@
  * What's in here?
  *  - function to render Inventory Box
  *
- * Ideas:
- *  - function to position multiple boxes?
- *  - fix math for inventory in asteroids
  *
  */
-
 #include "header.h"
-void renderInventory(Inventory box, int nbox, int posX) {
-    int red;
-    if (nbox > 0 ) {
-        red = 0;
-    } else {
-        red = 74;
-    }
+void renderInventory(Inventory box, int nbox, int posX, int IBOX) {
     glPushMatrix();
-        glColor3ub(red, 78, 105);
+        if (nbox <= IBOX) //boxes
+            glColor3ub(34, 34, 59);
+        if (nbox > IBOX) //border
+            glColor3ub(74, 78, 105);
         glTranslatef(posX, box.pos[1], 0.0f);
         glBegin(GL_QUADS);
             glVertex2f(-box.w, -box.h);
